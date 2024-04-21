f8858fa16e1650fd3bed641b4e03899604b53db3
Mariam Laaroussi Ramos <alu0101441764@ull.edu.es>
2024-04-21 23:55:56 +0000

    cambios

---
+++ tests/card.spec.ts
@@ -0,0 +1,61 @@
+import 'mocha';
+import { expect } from 'chai';
+import request from 'request';
+
+// URL base del servidor
+const baseUrl = 'http://localhost:3000';
+
+describe('Cartas', function () {
+  // Prueba para la ruta GET /cards con ID de carta
+  describe('GET /cards con ID de carta', function () {
+    it('Debería devolver un error si la carta no existe', function (done) {
+      request.get(baseUrl + '/cards?username=test_user&id=999', function (error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  it('Debería devolver la información de la carta solicitada', function (done) {
+    request.get(baseUrl + '/cards?username=test_user&id=1', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  // Prueba para la ruta POST /cards
+  describe('POST /cards', function () {
+    it('Debería agregar una nueva carta al usuario', function (done) {
+      const nuevaCarta = {
+        id: 2,
+        name: "Black Lotus",
+        cost: 20000,
+        color: "Rojo",
+        cardType: "Tierra",
+        rarity: "Rara",
+        rulesText: "practicas todas las semanas",
+        marketValue: 1
+      };
+
+      request.post({
+        url: baseUrl + '/cards?username=test_user',
+        json: true,
+        body: nuevaCarta
+      }, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+  });
+
+  // Prueba para la ruta DELETE /cards
+  describe('DELETE /cards', function () {
+    it('Debería notificar un error al intentar eliminar una carta no existente', function (done) {
+      request.delete(baseUrl + '/cards?username=test_user&id=999', function (error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+});
