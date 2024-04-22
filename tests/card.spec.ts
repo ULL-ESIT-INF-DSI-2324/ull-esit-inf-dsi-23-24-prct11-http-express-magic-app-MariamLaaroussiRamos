e764b92bbcb05d940c00e2bf8516f6efe6121132
Mariam Laaroussi Ramos <alu0101441764@ull.edu.es>
2024-04-22 00:33:41 +0000

    test

--- tests/card.spec.ts
+++ tests/card.spec.ts
@@ -6,14 +6,14 @@ import request from 'request';
 const baseUrl = 'http://localhost:3000';
 
 describe('Cartas', function () {
-  describe('GET /cards con ID de carta', function () {
-    it('Debería devolver un error si la carta no existe', function (done) {
-      request.get(baseUrl + '/cards?username=test_user&id=999', function (error, response, body) {
-        expect(response.statusCode).to.equal(404);
-        done();
-      });
-    });
-  });
+  describe('GET /cards con ID de carta', function () {
+    it('Debería devolver un error si la carta no existe', function (done) {
+      request.get(baseUrl + '/cards?username=test_user&id=999', function (error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
 
   it('Debería devolver la información de la carta solicitada', function (done) {
     request.get(baseUrl + '/cards?username=test_user&id=1', function (error, response, body) {
@@ -22,37 +22,37 @@ describe('Cartas', function () {
     });
   });
 
-  describe('POST /cards', function () {
-    it('Debería agregar una nueva carta al usuario', function (done) {
-      const nuevaCarta = {
-        id: 2,
-        name: "Black Lotus",
-        cost: 20000,
-        color: "Rojo",
-        cardType: "Tierra",
-        rarity: "Rara",
-        rulesText: "practicas todas las semanas",
-        marketValue: 1
-      };
+  describe('POST /cards', function () {
+    it('Debería agregar una nueva carta al usuario', function (done) {
+      const nuevaCarta = {
+        id: 2,
+        name: "Black Lotus",
+        cost: 20000,
+        color: "Rojo",
+        cardType: "Tierra",
+        rarity: "Rara",
+        rulesText: "practicas todas las semanas",
+        marketValue: 1
+      };
 
-      request.post({
-        url: baseUrl + '/cards?username=test_user',
-        json: true,
-        body: nuevaCarta
-      }, function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-  });
+    request.post({
+      url: baseUrl + '/cards?username=test_user',
+      json: true,
+      body: nuevaCarta
+    }, function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
 
-  describe('DELETE /cards', function () {
-    it('Debería notificar un error al intentar eliminar una carta no existente', function (done) {
-      request.delete(baseUrl + '/cards?username=test_user&id=999', function (error, response, body) {
-        expect(response.statusCode).to.equal(404);
-        done();
-      });
-    });
-  });
+  describe('DELETE /cards', function () {
+    it('Debería notificar un error al intentar eliminar una carta no existente', function (done) {
+      request.delete(baseUrl + '/cards?username=test_user&id=999', function (error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
 
 });
